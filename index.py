0482d157f08897c4bf720fbfe59aa8b4847e9e3d
Vagrant Default User <vagrant@control>
2019-12-26 22:04:54 +0000

    amelioration d code

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
-""" ceci est un test flask"""
+#!/usr/bin/env python
+""" test"""
 
-from flask import Flask
+from flask import Flask, render_template
 APP = Flask(__name__)
 
 @APP.route('/')
 def hello():
     """Affiche Helleo"""
 
-    return "Hello World!"
+    return render_template("home.html", message = "Hello Amadou, Welcome in your favorite page!")
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=5000)
