e32f1df698c327b2cc67bbe6ab27e592a53980f0
Vagrant Default User <vagrant@control>
2019-12-26 23:06:11 +0000

    login page

--- index.py
+++ index.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 """ test"""
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 APP = Flask(__name__)
 
 @APP.route('/')
 def hello():
     """Affiche Helleo"""
 
-    return render_template("home.html", message="Hello Amadou, Welcome in your favorite page!")
+    return render_template("home.html")
+
+@APP.route('/', methods=['POST'])
+def text_box():
+    text = request.form['text']
+    processed_text = text.upper()
+    return render_template("page_suivante.html", message = processed_text)
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=5000)
