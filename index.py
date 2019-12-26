9c054453531971f3cffe6ab43709c913c786752c
Vagrant Default User <vagrant@control>
2019-12-26 21:35:32 +0000

    correction index.py

--- index.py
+++ index.py
@@ -3,9 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@App.route('/')
 def hello():
+    """Affiche Helleo"""
+
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    """main fucntion"""
+    App.run(host="0.0.0.0", port=5000)
